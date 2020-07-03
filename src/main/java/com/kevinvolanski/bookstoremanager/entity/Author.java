ff8f6cebbfe7e3e8a91c73bf4a63790b8670399b
Kevin Volanski <kvolanski@gmail.com>
2020-07-02 21:44:51 -0300

    Developed project entities

---
+++ src/main/java/com/kevinvolanski/bookstoremanager/entity/Author.java
@@ -0,0 +1,25 @@
+package com.kevinvolanski.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private Integer age;
+
+}
